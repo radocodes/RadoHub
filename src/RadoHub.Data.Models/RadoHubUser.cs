28c08f3efa66952409285db8c6ab7dbf41e8fcd5
Radoslav Yordanov <yordanovus@yahoo.com>
2019-11-10 15:13:57 +0200

    Add Data Model Layer as separate project in the solution - RadoHub.Data.Models.csproj and NuGet package: Microsoft.AspNetCore.Identity.EntityFrameworkCore; Add RadoHubUser.cs with basic properties.
    Signed-off-by: Radoslav Yordanov

---
+++ src/RadoHub.Data.Models/RadoHubUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RadoHub.Data.Models
+{
+    public class RadoHubUser : IdentityUser
+    {
+        public RadoHubUser()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public string Company { get; set; }
+    }
+}
