c1cff31d650bdb1d05aebc48dcca02f62ac564da
Radoslav Yordanov <yordanov1234@gmail.com>
2019-12-08 15:11:54 +0200

    Added "About" IActionResult at HomeController and "About" view; Added profile picture and project picture at "About" view; Added FontAwasome reference and improved footer at Layout; Developed css style for "About" view and layout footer.
    Signed-off-by: Radoslav Yordanov

--- src/RadoHub.WebApp/Controllers/HomeController.cs
+++ src/RadoHub.WebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace RadoHub.WebApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
