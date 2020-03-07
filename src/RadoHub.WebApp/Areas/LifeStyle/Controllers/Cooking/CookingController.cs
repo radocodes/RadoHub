1bfd167d2a5869d3133c85e589bffcc2bb2c7341
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 17:20:37 +0200

    Introduced LifeStyleControllerBase

---
+++ src/RadoHub.WebApp/Areas/LifeStyle/Controllers/Cooking/CookingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RadoHub.WebApp.Areas.LifeStyle.Controllers.Cooking
+{
+    public class CookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
