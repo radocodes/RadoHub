70774441ab291b4a0041ad75dae5bf0e7c71c766
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 10:25:14 +0200

    Added Area default routing at startup

--- src/RadoHub.WebApp/Startup.cs
+++ src/RadoHub.WebApp/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -83,8 +83,14 @@ namespace RadoHub.WebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
         }
