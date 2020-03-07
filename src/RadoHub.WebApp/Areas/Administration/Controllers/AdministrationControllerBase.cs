44d6a839ed859acf3005eb8f3006f97d97e1e3cb
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 16:33:45 +0200

    Renamed AdministrationController to AdministrationControllerBase

---
+++ src/RadoHub.WebApp/Areas/Administration/Controllers/AdministrationControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RadoHub.Services.Constants;
+
+namespace RadoHub.WebApp.Areas.Administration.Controllers
+{
+    [Authorize(Roles = GlobalConstants.AdminRole)]
+    [Area("Administration")]
+    public class AdministrationControllerBase : Controller
+    {
+    }
+}
