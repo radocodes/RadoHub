3ba3c41064ee7d2982b31e9216caa5617cdcedc8
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 16:22:42 +0200

    Added Db migration AddCookingRecipes

--- src/RadoHub.Data/Migrations/20200307141422_AddCookingRecipes.Designer.cs
+++ src/RadoHub.Data/Migrations/20200307141422_AddCookingRecipes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RadoHub.WebApp.Data;
 
 namespace RadoHub.WebApp.Data.Migrations
 {
     [DbContext(typeof(RadoHubDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200307141422_AddCookingRecipes")]
+    partial class AddCookingRecipes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/RadoHub.Data/Migrations/20200307141422_AddCookingRecipes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RadoHub.WebApp.Data.Migrations
+{
+    public partial class AddCookingRecipes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CookingRecipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    ShortDescription = table.Column<string>(nullable: true),
+                    ExecutingTime = table.Column<TimeSpan>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<string>(nullable: true),
+                    CoverImageFileName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CookingRecipes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CookingRecipes");
+        }
+    }
+}
