502ce1cb3728a5bd315d4ed8feaf572a28abf57a
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 10:33:05 +0200

    Added HomeController in Administration area

---
+++ src/RadoHub.WebApp/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RadoHub.WebApp.Areas.Administration.Controllers
+{
+    public class HomeController : AdministrationController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
