e62bf2deb41e2b5f38e74abb6816bbedefe08d6d
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 15:54:26 +0200

    Added BaseModel class as basic DB model

---
+++ src/RadoHub.Data.Models/common/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.Data.Models
+{
+    public class BaseModel<T>
+    {
+        public T Id { get; set; }
+    }
+}
