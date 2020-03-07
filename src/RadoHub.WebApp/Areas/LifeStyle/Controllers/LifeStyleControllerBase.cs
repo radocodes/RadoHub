c7ae798500c35ea313c3524598517997a3e393f3
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-07 16:34:39 +0200

    Rename AdministrationController to AdministrationControllerBase at HomeController in Administration Area

--- src/RadoHub.WebApp/Areas/LifeStyle/Controllers/LifeStyleControllerBase.cs
+++ src/RadoHub.WebApp/Areas/LifeStyle/Controllers/LifeStyleControllerBase.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace RadoHub.WebApp.Areas.Administration.Controllers
+namespace RadoHub.WebApp.Areas.LifeStyle.Controllers
 {
-    public class HomeController : AdministrationControllerBase
+    public class LifeStyleControllerBase : Controller
     {
         public IActionResult Index()
         {
