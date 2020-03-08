4e5b39c0ba08a7f7717aa21215a93a3ea07ce456
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-08 17:49:23 +0200

    Introduced CookingRecipeService inherit ICookingRecipeService

---
+++ src/RadoHub.Services/Services/CookingRecipeService.cs
@@ -0,0 +1,51 @@
+﻿using RadoHub.Data.Models;
+using RadoHub.Data.Repositories.Contracts;
+using RadoHub.Services.Contracts;
+using RadoHub.ViewModels.CookingRecipe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadoHub.Services.Services
+{
+    public class CookingRecipeService : ICookingRecipeService
+    {
+        private readonly ICookingRecipeRepository cookingRecipeRepo;
+
+        public CookingRecipeService(ICookingRecipeRepository cookingRecipeRepository)
+        {
+            this.cookingRecipeRepo = cookingRecipeRepository;
+        }
+
+        public Task CreateCookingRecipeAsync(CreateCookingRecipeViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int cookingRecipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CookingRecipe> GetAllCookingRecipes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CookingRecipe GetCookingRecipeById(int cookingRecipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CookingRecipe> GetCookingRecipesByKeyWords(string userSearching)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCookingRecipeAsync(CookingRecipe updatingModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
