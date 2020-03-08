8177dd6181fb2daf503f56c3f3498ac39b3d33b5
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-08 18:35:03 +0200

    Introduced FileService class with implemented SaveImageFile method

---
+++ src/RadoHub.Services/Services/FileService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadoHub.Services.Services
+{
+    public class FileService
+    {
+        public async Task SaveImageFile(string fullPath, IFormFile imageFile)
+        {
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(stream);
+            };
+        }
+    }
+}
