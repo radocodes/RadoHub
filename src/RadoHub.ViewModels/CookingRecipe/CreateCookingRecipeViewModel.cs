d79116c11e2912d417f7e384869584e1ff56389c
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-08 17:45:04 +0200

    Added CreateCookingRecipeViewModel

---
+++ src/RadoHub.ViewModels/CookingRecipe/CreateCookingRecipeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RadoHub.ViewModels.CookingRecipe
+{
+    public class CreateCookingRecipeViewModel
+    {
+        [Display(Name = "Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Short description")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "Executing time")]
+        public TimeSpan ExecutingTime { get; set; }
+
+        [Display(Name = "Products")]
+        [Required]
+        public IEnumerable<string> Products { get; set; }
+
+        [Display(Name = "Content")]
+        [Required]
+        public string Content { get; set; }
+
+        public string CreatorId { get; set; }
+
+        [Display(Name = "Cover image")]
+        public IFormFile CoverImage { get; set; }
+
+        [Display(Name = "Images")]
+        public IEnumerable<IFormFile> Images { get; set; }
+
+        [Display(Name = "Hashtags")]
+        public IEnumerable<string> Hashtags { get; set; }
+    }
+}
