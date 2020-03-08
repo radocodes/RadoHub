c53b89766dc05b234d235e8eafc8a0196079f9c4
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-08 17:48:29 +0200

    Introduced ICookingRecipeService with basic methods

---
+++ src/RadoHub.Services/Contracts/ICookingRecipeService.cs
@@ -0,0 +1,24 @@
+﻿using RadoHub.Data.Models;
+using RadoHub.ViewModels.CookingRecipe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadoHub.Services.Contracts
+{
+    public interface ICookingRecipeService
+    {
+        List<CookingRecipe> GetAllCookingRecipes();
+
+        List<CookingRecipe> GetCookingRecipesByKeyWords(string userSearching);
+
+        CookingRecipe GetCookingRecipeById(int cookingRecipeId);
+
+        Task CreateCookingRecipeAsync(CreateCookingRecipeViewModel model);
+
+        Task UpdateCookingRecipeAsync(CookingRecipe updatingModel);
+
+        Task DeleteAsync(int cookingRecipeId);
+    }
+}
