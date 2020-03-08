6c23fb966c178473cf347294629c654134b2fed0
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-08 13:08:43 +0200

    Added ICookingRecipeRepository interface with basics functionality

---
+++ src/RadoHub.Data/Repositories/Contracts/ICookingRecipeRepository.cs
@@ -0,0 +1,23 @@
+﻿using RadoHub.Data.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.Data.Repositories.Contracts
+{
+    public interface ICookingRecipeRepository
+    {
+        ICollection<CookingRecipe> GetAllCookingRecipes();
+
+        ICollection<CookingRecipe> GetCookingRecipesByHashTags();
+
+        CookingRecipe GetCookingRecipeById(int CookingRecipeId);
+
+        CookingRecipe CreateCookingRecipe(CookingRecipe cookingRecipe);
+
+        void UpdateCookingRecipe(CookingRecipe UpdatingModel);
+
+        void Delete(CookingRecipe cookingRecipe);
+    }
+}
