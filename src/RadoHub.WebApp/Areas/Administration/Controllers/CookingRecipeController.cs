69a79fc9c6f0e9e1205525ef487ba41e7919bd6e
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-15 17:33:58 +0200

    Introdeced and implemented IActionResult DeleteCookingRecipe

--- src/RadoHub.WebApp/Areas/Administration/Controllers/CookingRecipeController.cs
+++ src/RadoHub.WebApp/Areas/Administration/Controllers/CookingRecipeController.cs
@@ -11,13 +11,38 @@ namespace RadoHub.WebApp.Areas.Administration.Controllers
         {
             this.cookingRecipeService = cookingRecipeService;
         }
+
         public IActionResult Index()
         {
+            if (TempData["statusMessage"] != null)
+            {
+                ViewData["statusMessage"] = TempData["statusMessage"];
+            }
+
             var viewModel = new AdminGetAllRecipesViewModel();
 
             viewModel.AllCookingRecipes = this.cookingRecipeService.GetAllCookingRecipes();
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCookingRecipe(int id)
+        {
+            try
+            {
+                this.cookingRecipeService.DeleteAsync(id).GetAwaiter().GetResult();
+
+                TempData["statusMessage"] = "Cooking recipe was removed successfully!";
+                return RedirectToAction("Index", "CookingRecipe");
+            }
+            catch (System.Exception exeption)
+            {
+                TempData["statusMessage"] = $"Action Failed! | {exeption.Message}";
+                return RedirectToAction("Index", "CookingRecipe");
+            }
+            
+
         }
     }
 }
