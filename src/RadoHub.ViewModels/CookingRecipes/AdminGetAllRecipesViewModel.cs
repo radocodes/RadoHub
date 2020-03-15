5d370e630c4ed8a3aa740f22e1fe2d8af9dbf023
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-15 17:10:02 +0200

    Renamed CookingRecipe folder to CookingRecipes at RadoHub.ViewModels

---
+++ src/RadoHub.ViewModels/CookingRecipes/AdminGetAllRecipesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RadoHub.Data.Models;
+
+namespace RadoHub.ViewModels.CookingRecipes
+{
+    public class AdminGetAllRecipesViewModel
+    {
+        public IEnumerable<Data.Models.CookingRecipe> AllCookingRecipes { get; set; }
+    }
+}
