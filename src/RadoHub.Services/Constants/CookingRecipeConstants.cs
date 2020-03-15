484043e058dece70b9f97ca6a0ec6a32317bc22f
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-15 09:49:05 +0200

    Added CookingRecipeConstants class and implemented some basic constants

---
+++ src/RadoHub.Services/Constants/CookingRecipeConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.Services.Constants
+{
+    public class CookingRecipeConstants
+    {
+        public const string ProdImageFolderPath = @"wwwroot\images\production";
+        public const string StageImageFolderPath = @"wwwroot\images\stage";
+        public const string CookingRecipesImageFolderName = @"cookingRecipes";
+        public const string CoverImagefolderName = "cover";
+    }
+}
