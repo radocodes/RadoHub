23113222dd8d73ca1859a33aad5eda034056d369
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-15 10:47:57 +0200

    Added FileExtensions class and introduced imageExtansion const inside

---
+++ src/RadoHub.Services/Constants/FileExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.Services.Constants
+{
+    public class FileExtensions
+    {
+        public const string ImageExtension = ".jpg";
+    }
+}
