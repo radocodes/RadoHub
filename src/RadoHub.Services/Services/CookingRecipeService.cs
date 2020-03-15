b4e3f8a238a36fac5e3078d507b31d32cffb5a53
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-15 10:48:26 +0200

    Implemented CreateCookingRecipe at CookingRecipeService

--- src/RadoHub.Services/Services/CookingRecipeService.cs
+++ src/RadoHub.Services/Services/CookingRecipeService.cs
@@ -1,9 +1,11 @@
 ﻿using RadoHub.Data.Models;
 using RadoHub.Data.Repositories.Contracts;
+using RadoHub.Services.Constants;
 using RadoHub.Services.Contracts;
 using RadoHub.ViewModels.CookingRecipe;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,15 +14,68 @@ namespace RadoHub.Services.Services
     public class CookingRecipeService : ICookingRecipeService
     {
         private readonly ICookingRecipeRepository cookingRecipeRepo;
+        private readonly IUserAccountService userAccountService;
+        private readonly IFileService fileService;
 
-        public CookingRecipeService(ICookingRecipeRepository cookingRecipeRepository)
+        public CookingRecipeService
+            (ICookingRecipeRepository cookingRecipeRepository,
+            IUserAccountService userAccountService,
+            IFileService fileService)
         {
             this.cookingRecipeRepo = cookingRecipeRepository;
+            this.userAccountService = userAccountService;
+            this.fileService = fileService;
         }
 
-        public Task CreateCookingRecipeAsync(CreateCookingRecipeViewModel model)
+        public void CreateCookingRecipe(CreateCookingRecipeViewModel model)
         {
-            throw new NotImplementedException();
+
+            var cookingRecipe = new CookingRecipe()
+            {
+                Title = model.Title,
+                ShortDescription = model.ShortDescription,
+                ExecutingTime = model.ExecutingTime,
+                Products = model.Products.ToHashSet(),
+                Content = model.Content,
+                CreationDate = DateTime.UtcNow,
+                LastModifiedAt = DateTime.UtcNow,
+                CreatorId = model.CreatorId,
+                Hashtags = model.Hashtags.ToHashSet()
+            };
+
+            var newCookingRecipeId = this.cookingRecipeRepo
+                .CreateCookingRecipeAsync(cookingRecipe)
+                .GetAwaiter().GetResult();
+
+            var sb = new StringBuilder();
+            sb.Append(CookingRecipeConstants.StageImageFolderPath);
+            sb.Append(CookingRecipeConstants.CookingRecipesImageFolderName);
+            sb.Append(newCookingRecipeId);
+
+            var currRecipeAllimagesPath = sb.ToString();
+
+            sb.Append(CookingRecipeConstants.CoverImagefolderName);
+
+            var currCoverImagePath = sb.ToString();
+
+            this.fileService.CreateDirectory(currCoverImagePath);
+
+            var coverImgFileName = $"{Guid.NewGuid().ToString()}{FileExtensions.ImageExtension}";
+
+            if (model.CoverImage != null)
+            {
+                this.fileService.SaveImageFile($"{currCoverImagePath}{coverImgFileName}", model.CoverImage);
+            }
+
+            if (model.Images != null && model.Images.Count() > 0)
+            {
+                foreach (var image in model.Images)
+                {
+                    var imageFileName = $"{Guid.NewGuid().ToString()}{FileExtensions.ImageExtension}";
+
+                    this.fileService.SaveImageFile($"{currRecipeAllimagesPath}{imageFileName}", model.CoverImage);
+                }
+            }
         }
 
         public Task DeleteAsync(int cookingRecipeId)
