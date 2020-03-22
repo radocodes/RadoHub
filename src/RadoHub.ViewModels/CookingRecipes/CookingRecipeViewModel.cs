f8e48cc54d4efbf56c14eee9cbee5ec4f0fe491c
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-22 12:12:12 +0200

    Added and implemented CookingRecipeViewModel and CookingRecipesViewModel

---
+++ src/RadoHub.ViewModels/CookingRecipes/CookingRecipeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using RadoHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.ViewModels.CookingRecipes
+{
+    public class CookingRecipeViewModel : BaseModel<int>
+    {
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public TimeSpan ExecutingTime { get; set; }
+
+        public ICollection<string> Products { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastModifiedAt { get; set; }
+
+        public string CoverImageFileName { get; set; }
+
+        public ICollection<string> ImagesFileNames { get; set; }
+
+        public ICollection<string> Hashtags { get; set; }
+    }
+}
---
+++ src/RadoHub.ViewModels/CookingRecipes/CookingRecipesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.ViewModels.CookingRecipes
+{
+    public class CookingRecipesViewModel
+    {
+        public IEnumerable<CookingRecipeViewModel> Recipes { get; set; }
+    }
+}
