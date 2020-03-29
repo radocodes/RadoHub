f90c7f6c9c08cda83c21d4eb2b59ca9d10e2a7dd
Radoslav Yordanov <yordanov1234@gmail.com>
2020-03-29 09:50:46 +0300

    Added DB migration - updated CookingRecipeModel following props to string: Products, ImagesFileNames, Hashtags, EditorsUsernames

---
+++ src/RadoHub.Data/Migrations/20200329062146_UpdateCookingRecipeModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RadoHub.WebApp.Data.Migrations
+{
+    public partial class UpdateCookingRecipeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EditorsUsernames",
+                table: "CookingRecipes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hashtags",
+                table: "CookingRecipes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagesFileNames",
+                table: "CookingRecipes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Products",
+                table: "CookingRecipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EditorsUsernames",
+                table: "CookingRecipes");
+
+            migrationBuilder.DropColumn(
+                name: "Hashtags",
+                table: "CookingRecipes");
+
+            migrationBuilder.DropColumn(
+                name: "ImagesFileNames",
+                table: "CookingRecipes");
+
+            migrationBuilder.DropColumn(
+                name: "Products",
+                table: "CookingRecipes");
+        }
+    }
+}
