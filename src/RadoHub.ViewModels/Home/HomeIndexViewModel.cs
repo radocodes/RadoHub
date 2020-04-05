8ec343d56bb72a1a2ed91973760662d6e6f0ec3a
Radoslav Yordanov <yordanov1234@gmail.com>
2020-04-05 15:36:23 +0300

    Added HomeIndexViewModel

---
+++ src/RadoHub.ViewModels/Home/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadoHub.ViewModels.CookingRecipes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.ViewModels.Home
+{
+    public class HomeIndexViewModel
+    {
+        public CookingRecipesViewModel CookingRecipesModel { get; set; }
+    }
+}
