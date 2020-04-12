80dd520bc842577931a2c62f40f1ef6dd9afda09
Radoslav Yordanov <yordanov1234@gmail.com>
2020-04-12 10:54:00 +0300

    Added and implement EditRecipeViewModel

---
+++ src/RadoHub.ViewModels/CookingRecipes/EditRecipeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.ViewModels.CookingRecipes
+{
+    public class EditRecipeViewModel : CreateRecipeViewModel
+    {
+        public int Id { get; set; }
+    }
+}
