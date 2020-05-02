5d7f601151c5a18ee3ed0ff7ae1f97f0da110f08
Radoslav Yordanov <yordanov1234@gmail.com>
2020-05-02 18:08:03 +0300

    Added UpdateImageFile method and improve CreateDirectory method at FileService

--- src/RadoHub.Services/Services/FileService.cs
+++ src/RadoHub.Services/Services/FileService.cs
@@ -16,11 +16,11 @@ namespace RadoHub.Services.Services
             {
                 Directory.CreateDirectory(fullPath);
             }
-            else
-            {
-                var exeptionMessage = "Operation Failed! Directory you tried to create already exist";
-                throw new Exception(exeptionMessage);
-            }
+            //else
+            //{
+            //    var exeptionMessage = "Operation Failed! Directory you tried to create already exist";
+            //    throw new Exception(exeptionMessage);
+            //}
         }
 
         public void DeleteDirectory(string fullPath)
@@ -55,6 +55,11 @@ namespace RadoHub.Services.Services
                 var exeptionMessage = "Operation Failed! The file you tried to delete is not found";
                 throw new FileNotFoundException(exeptionMessage, fullPath);
             }
+        }
+
+        public async Task UpdateImageFile(string fullPath, IFormFile imageFile)
+        {
+            await this.SaveImageFile(fullPath, imageFile);
         }
     }
 }
