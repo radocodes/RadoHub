a1e559f6f6cbfd213505ae0cdad4f89cfa1e132c
Radoslav Yordanov <yordanov1234@gmail.com>
2020-08-15 17:25:54 +0300

    Improved code and removed unnecessary usings

--- src/RadoHub.Services/Constants/RadoHubEnvironments.cs
+++ src/RadoHub.Services/Constants/RadoHubEnvironments.cs
@@ -4,6 +4,5 @@
     {
         public const string Development = "Development";
         public const string Production = "Production";
-
     }
 }
--- src/RadoHub.Services/Contracts/ICookingRecipeService.cs
+++ src/RadoHub.Services/Contracts/ICookingRecipeService.cs
@@ -1,8 +1,6 @@
 ﻿using RadoHub.Data.Models;
 using RadoHub.ViewModels.CookingRecipes;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RadoHub.Services.Contracts
