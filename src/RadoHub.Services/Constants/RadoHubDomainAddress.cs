7d6e614e907c4925269c02056577117a4be26c2d
Radoslav Yordanov <yordanov1234@gmail.com>
2020-08-15 13:07:40 +0300

    Added RadoHubDomainAddress class at RadoHub.Services\Constants

---
+++ src/RadoHub.Services/Constants/RadoHubDomainAddress.cs
@@ -0,0 +1,8 @@
+﻿namespace RadoHub.Services.Constants
+{
+    public class RadoHubDomainAddress
+    {
+        public const string LocalHost = "https://localhost:5001";
+        public const string ProductionAddress = "http://radohub.com/";
+    }
+}
