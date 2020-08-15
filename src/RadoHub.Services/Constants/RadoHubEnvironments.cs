e65f94cbfc665a0c5a6bf7372089f22aebc0c630
Radoslav Yordanov <yordanov1234@gmail.com>
2020-08-15 12:45:06 +0300

    Renamed Environment class to RadoHubEnvironments at RadoHub.Services.Constants

---
+++ src/RadoHub.Services/Constants/RadoHubEnvironments.cs
@@ -0,0 +1,9 @@
+﻿namespace RadoHub.Services.Constants
+{
+    public class RadoHubEnvironments
+    {
+        public const string Development = "Development";
+        public const string Production = "Production";
+
+    }
+}
