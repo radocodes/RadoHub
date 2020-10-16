5122d02dae278c4e08ae86064a13a82e85189626
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-16 19:14:56 +0300

    Added LibraryViewModel

---
+++ src/RadoHub.ViewModels/IT/UsefulTools/LibraryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CloudinaryDotNet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadoHub.ViewModels.IT.UsefulTools
+{
+    public class LibraryViewModel
+    {
+        public Cloudinary Cloudinary { get; set; }
+    }
+}
