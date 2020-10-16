7b766a4549ddd09782ea810c0dc8edcb5794ceb3
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-16 19:07:07 +0300

    Added Areas.cs constants

---
+++ src/RadoHub.Services/Constants/Areas.cs
@@ -0,0 +1,10 @@
+﻿namespace RadoHub.Services.Constants
+{
+    public class Areas
+    {
+        // Web app areas:
+        public const string Administration = "Administration";
+        public const string IT = "IT";
+        public const string Lifestyle = "Lifestyle";
+    }
+}
