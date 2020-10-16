2e7d21bac580d4e4a675a35b25cf5b25f7748831
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-16 19:09:37 +0300

    Created IT area and ITControllerBase

---
+++ src/RadoHub.WebApp/Areas/IT/Controllers/ITControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadoHub.WebApp.Areas.IT.Controllers
+{
+    [Area(Services.Constants.Areas.IT)]
+    public class ITControllerBase : Controller
+    {
+    }
+}
