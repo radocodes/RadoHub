3ff8cf9e473c9d4c5aa203bac4b718de554c8e8c
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-16 19:10:09 +0300

    Added UsefulToolsController at IT area

---
+++ src/RadoHub.WebApp/Areas/IT/Controllers/UsefulToolsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RadoHub.Services.Constants;
+using RadoHub.Services.Contracts;
+using RadoHub.ViewModels.IT.UsefulTools;
+
+namespace RadoHub.WebApp.Areas.IT.Controllers
+{
+    public class UsefulToolsController : ITControllerBase
+    {
+        private readonly ICloudinaryService cloudinaryService;
+
+        public UsefulToolsController(ICloudinaryService cloudinaryService)
+        {
+            this.cloudinaryService = cloudinaryService;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Library()
+        {
+            LibraryViewModel viewModel = new LibraryViewModel()
+            {
+                Cloudinary = this.cloudinaryService.GetCloudinaryInstance()
+            };
+
+            ViewData["UnderContructionImageFileName"] = GlobalConstants.UnderConstructionImageFileName;
+
+            return View(viewModel);
+        }
+    }
+}
