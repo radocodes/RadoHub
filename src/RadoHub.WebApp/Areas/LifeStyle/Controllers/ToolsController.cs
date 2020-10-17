2b2f91898666ee661897281e79b5bd952ac4ee00
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-17 11:44:57 +0300

    Added ToolsController and RakiaCalculator action method (Lifestyle area)

---
+++ src/RadoHub.WebApp/Areas/LifeStyle/Controllers/ToolsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RadoHub.Services.Contracts;
+using RadoHub.ViewModels.Lifestyle.Tools;
+
+namespace RadoHub.WebApp.Areas.LifeStyle.Controllers
+{
+    public class ToolsController: LifeStyleControllerBase
+    {
+        private readonly ICloudinaryService cloudinaryService;
+
+        public ToolsController(ICloudinaryService cloudinaryService)
+        {
+            this.cloudinaryService = cloudinaryService;
+        }
+
+        public IActionResult RakiaCalculator()
+        {
+            const string headerImageFileName = "jim-harris-zDlusnb3G3Q-unsplash_czwkgt";
+
+            var viewModel = new RakiaCalculatorViewModel()
+            {
+                Cloudinary = this.cloudinaryService.GetCloudinaryInstance()
+            };
+
+            ViewData["HeaderImageFileName"] = headerImageFileName;
+
+            return View(viewModel);
+        }
+    }
+}
