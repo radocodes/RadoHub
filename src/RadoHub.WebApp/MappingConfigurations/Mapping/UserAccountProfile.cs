cd08147eb81cfcb616c8d7fa924f7e999510528b
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-24 18:13:27 +0300

    Added UserAccountProfile as Automapper profile at web project

---
+++ src/RadoHub.WebApp/MappingConfigurations/Mapping/UserAccountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RadoHub.Data.Models;
+using RadoHub.ViewModels.UserAccount;
+
+namespace RadoHub.Services.MappingConfigurations.Mapping
+{
+    public class UserAccountProfile : Profile
+    {
+        public UserAccountProfile()
+        {
+            this.CreateMap<RadoHubUser, UserAccountAdminViewModel>();
+        }
+    }
+}
