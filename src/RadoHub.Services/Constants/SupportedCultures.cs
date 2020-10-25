2fe310bb8d53569cd7f8ea525b3ee20b5b2f2971
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-25 16:26:38 +0200

    Added SupportedCultures.cs lookups helper

---
+++ src/RadoHub.Services/Constants/SupportedCultures.cs
@@ -0,0 +1,15 @@
+﻿using RadoHub.Data.Models;
+
+namespace RadoHub.Services.Constants
+{
+    public class SupportedCultures
+    {
+        public static Culture[] GetAll => new[] { Bulgarian, English };
+
+        public static Culture Bulgarian => new Culture("BG", "bg");
+
+        public static Culture English => new Culture("EN", "en-US");
+
+
+    }
+}
