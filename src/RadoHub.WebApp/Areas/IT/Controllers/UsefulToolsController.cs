cce796cdd555c2758c70d513208aa46d6160499b
Radoslav Yordanov <yordanov1234@gmail.com>
2020-11-27 21:41:01 +0200

    Added SvgSpriteSplitter and DownloadSvgSpriteSplitter endpoints at UsefulToolsController

--- src/RadoHub.WebApp/Areas/IT/Controllers/UsefulToolsController.cs
+++ src/RadoHub.WebApp/Areas/IT/Controllers/UsefulToolsController.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using RadoHub.Services.Constants;
 using RadoHub.Services.Contracts;
 using RadoHub.ViewModels.IT.UsefulTools;
+using System.IO;
 
 namespace RadoHub.WebApp.Areas.IT.Controllers
 {
     public class UsefulToolsController : ITControllerBase
     {
         private readonly ICloudinaryService cloudinaryService;
-
-        public UsefulToolsController(ICloudinaryService cloudinaryService)
+        private readonly IWebHostEnvironment environment;
+        public UsefulToolsController(ICloudinaryService cloudinaryService, IWebHostEnvironment environment)
         {
             this.cloudinaryService = cloudinaryService;
+            this.environment = environment;
         }
 
         //public IActionResult Index()
@@ -29,6 +33,37 @@ namespace RadoHub.WebApp.Areas.IT.Controllers
             ViewData["UnderContructionImageFileName"] = GlobalConstants.UnderConstructionImageFileName;
 
             return View(viewModel);
+        }
+
+        public IActionResult SvgSpriteSplitter()
+        {
+            return this.View();
+        }
+
+        public IActionResult DownloadSvgSpriteSplitter(string type)
+        {
+            var existingTypes = new string []{ "win64", "win86", "linux64" };
+
+            foreach (var item in existingTypes)
+            {
+                if (item == type)
+                {
+                    var wwwwrootPath = this.environment.WebRootPath;
+                    var filePath = $"{wwwwrootPath}/download/apps/svg-sprite-splitter/{type}.7z";
+
+                    string contentType;
+                    new FileExtensionContentTypeProvider().TryGetContentType(filePath, out contentType);
+                    if (contentType == null)
+                    {
+                        contentType = "application/octet-stream";
+                    }
+
+                    var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    return File(fileStream, contentType, $"SVG_Sprite_Splitter_{type}.7z");
+                }
+            }
+
+            return BadRequest();
         }
     }
 }
