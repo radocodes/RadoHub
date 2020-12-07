c3f29caff92aee9e6d128682e521d22bc5bef482
Radoslav Yordanov <yordanov1234@gmail.com>
2020-12-07 21:12:38 +0200

    Added CookingRecipeProfile as Automapper profile

---
+++ src/RadoHub.WebApp/MappingConfigurations/Mapping/CookingRecipeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RadoHub.Data.Models;
+using RadoHub.ViewModels.CookingRecipes;
+
+namespace RadoHub.WebApp.MappingConfigurations.Mapping
+{
+    public class CookingRecipeProfile : Profile
+    {
+        public CookingRecipeProfile()
+        {
+            this.CreateMap<CookingRecipe, CookingRecipeViewModel>();
+        }
+    }
+}
