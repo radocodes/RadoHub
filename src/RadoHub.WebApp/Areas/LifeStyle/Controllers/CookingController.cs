11a253b5c7697e028146514779244d57ac296988
Radoslav Yordanov <yordanov1234@gmail.com>
2020-12-07 21:14:30 +0200

    Added search engine for Cooking-Recipes at Cooking controller and Cooking / Index.cshtml

--- src/RadoHub.WebApp/Areas/LifeStyle/Controllers/CookingController.cs
+++ src/RadoHub.WebApp/Areas/LifeStyle/Controllers/CookingController.cs
@@ -1,25 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RadoHub.Data.Repositories.Contracts;
 using RadoHub.Services.Contracts;
 using RadoHub.ViewModels.CookingRecipes;
+using System.Collections.Generic;
 
 namespace RadoHub.WebApp.Areas.LifeStyle.Controllers.Cooking
 {
     public class CookingController : LifeStyleControllerBase
     {
+        private readonly ICookingRecipeRepository cookingRecipeRepo;
         private readonly ICookingRecipeService cookingRecipeService;
         private readonly ICloudinaryService cloudinaryService;
+        private readonly IMapper mapper;
 
-        public CookingController(ICookingRecipeService cookingRecipeService, ICloudinaryService cloudinaryService)
+        public CookingController(
+            ICookingRecipeRepository cookingRecipeRepository,
+            ICookingRecipeService cookingRecipeService,
+            ICloudinaryService cloudinaryService,
+            IMapper mapper)
         {
             this.cookingRecipeService = cookingRecipeService;
             this.cloudinaryService = cloudinaryService;
+            this.cookingRecipeRepo = cookingRecipeRepository;
+            this.mapper = mapper;
+
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string currentFilter, string searchString, int? pageNumber)
         {
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
             var model = new CookingRecipesViewModel();
 
-            model.Recipes = this.cookingRecipeService.GetAllRecipesAsPublic();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model.Recipes = mapper.Map<List<CookingRecipeViewModel>>(this.cookingRecipeRepo.GetAllCookingRecipesByKeyword(searchString));
+            }
+            else
+            {
+                model.Recipes = mapper.Map<List<CookingRecipeViewModel>>(this.cookingRecipeRepo.GetAllCookingRecipes());
+            }
+            
             model.Cloudinary = this.cloudinaryService.GetCloudinaryInstance();
 
             return this.View(model);
