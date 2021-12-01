d12509bfc22c9d4d35664d6292b2e18f218a44fe
Radoslav Yordanov <yordanov1234@gmail.com>
2021-12-01 18:02:45 +0200

    Added Migration ChangedModelProperties

---
+++ src/RadoHub.Data/Migrations/20211201155305_ChangedModelProperties.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RadoHub.WebApp.Data.Migrations
+{
+    public partial class ChangedModelProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
